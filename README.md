06a35b75179b1cf4a352f098e059b956c25126a4
nigh <jiyucheng007@gmail.com>
2018-03-10 14:37:31 +0800

    首个可用版本
    Signed-off-by: nigh <jiyucheng007@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # raspi-jlink-downloader
-一个运行于树莓派上的jlink烧录器
+一个运行于树莓派上的jlink量产烧录器
+
+![完整系统](system.jpg)
+
+
+
+![原理图](hw/circuit.png)
+
+## 安装
+
+1. 从jlink官网下载jlink for arm包，在树莓派上解压。
+1. 使用`ln -s`在`/sbin`目录下建立`JLinkExe`的软链接，这样就可以使用`sudo JLinkExe`直接启动。
+1. 在`windows`上以`arm5`为目标平台交叉编译出二进制程序（如出现gcc依赖问题，尝试关闭`CGO_ENABLE`选项），这样可以省去在树莓派上配置`golang`环境的麻烦。
+1. 复制编译好的二进制程序到树莓派目录。使用`chmod +x`赋予可执行权限。
+1. 使用`sudo`身份运行程序。
+
+## 部署
+
+1. 使用`sudo vi /etc/rc.local`编辑文件，在最后`exit`之前加上一行`exec 程序路径`，即可实现自动启动。
+1. 如果需要开机自动登陆，可使用`raspi-config`配置自动登陆。
+
+## 说明
+
+项目`hw`目录下为硬件设计文件，`src`目录为烧录程序`golang`源码。
+
---
+++ src/main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"strings"
+
+	rpio "github.com/stianeikeland/go-rpio"
+)
+
+var cmd bytes.Buffer
+
+//device nrf52832_xxaa
+//si 1
+//speed 8000
+//r
+//h
+//loadfile xxx.hex
+//setpc 0
+//g
+//q
+var hexfile string = "/home/pi/Downloads/test.hex"
+var cmdfile string = "test.jlink"
+
+func init() {
+	runtime.GOMAXPROCS(1)
+	rand.Seed(time.Now().UnixNano())
+	cmd.WriteString("device nrf52832_xxaa\n")
+	cmd.WriteString("si 1\n")
+	cmd.WriteString("speed 8000\n")
+	cmd.WriteString("r\nh\n")
+	cmd.WriteString("loadfile " + hexfile + "\n")
+	cmd.WriteString("setpc 0\n")
+	cmd.WriteString("g\nq\n")
+	if checkFileIsExist(cmdfile) == true {
+		os.Remove(cmdfile)
+	}
+	f, _ := os.Create(cmdfile) //创建文件
+	io.WriteString(f, cmd.String())
+	f.Close()
+}
+
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+var button rpio.Pin
+var button2 rpio.Pin
+
+var R rpio.Pin
+var G rpio.Pin
+var B rpio.Pin
+var Buzzer rpio.Pin
+
+func main() {
+	fmt.Printf("Kurumi Programming System Initial...")
+	if rpio.Open() == nil {
+		fmt.Println("[OK]")
+	} else {
+		fmt.Println("[ERROR]")
+		return
+	}
+	defer rpio.Close()
+	Buzzer = rpio.Pin(4)
+	R = rpio.Pin(23)
+	G = rpio.Pin(24)
+	B = rpio.Pin(25)
+
+	Buzzer.Output()
+	R.Output()
+	G.Output()
+	B.Output()
+
+	R.Low()
+	G.Low()
+	B.Low()
+	Buzzer.Low()
+	<-time.After(777 * time.Millisecond)
+
+	Buzzer.High()
+	R.High()
+	G.High()
+	B.High()
+
+	heartbeat := time.Tick(1737 * time.Millisecond)
+	go func() {
+		for {
+			<-heartbeat
+			B.Toggle()
+		}
+	}()
+
+	button = rpio.Pin(27)
+	button.Input()
+	button.PullUp()
+	button2 = rpio.Pin(16)
+	button2.Input()
+	button2.PullUp()
+
+	var keyevent = make(chan string)
+
+	go keyscan(keyevent)
+
+	go eventhandler(keyevent)
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
+
+func download() {
+	G.High()
+	R.High()
+	var esc = make(chan bool)
+	go func(esc chan bool) {
+		for {
+			tick := time.After(100 * time.Millisecond)
+			select {
+			case <-tick:
+				B.Toggle()
+			case <-esc:
+				return
+			}
+		}
+	}(esc)
+	exe := exec.Command("JLinkExe", cmdfile)
+	//	err := exe.Run()
+	//	if err != nil {
+	//		fmt.Print(err)
+	//	}
+	out, err := exe.Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	esc <- false
+	fmt.Println(string(out))
+	if strings.Contains(string(out), "Verifying") {
+		fmt.Println("[Success]")
+		go func() {
+			G.Low()
+			Buzzer.Low()
+			<-time.After(400 * time.Millisecond)
+			Buzzer.High()
+		}()
+	} else {
+		fmt.Println("[Failure]")
+		go func() {
+			R.Low()
+			for j := 1; j <= 7; j++ {
+				Buzzer.Low()
+				<-time.After(100 * time.Millisecond)
+				Buzzer.High()
+				<-time.After(100 * time.Millisecond)
+			}
+		}()
+	}
+}
+
+func keyscan(event chan string) {
+	var state rpio.State = button.Read()
+	var state2 rpio.State = button2.Read()
+	scanner := time.NewTicker(40 * time.Millisecond)
+	for {
+		select {
+		case <-scanner.C:
+			temp := button.Read()
+			if state != temp {
+				state = temp
+				if state == 1 {
+					event <- "B1 RisingEdge"
+				} else {
+					event <- "B1 FallingEdge"
+				}
+			}
+			temp = button2.Read()
+			if state2 != temp {
+				state2 = temp
+				if state2 == 1 {
+					event <- "B2 RisingEdge"
+				} else {
+					event <- "B2 FallingEdge"
+				}
+			}
+		}
+	}
+}
+
+func eventhandler(event chan string) {
+	for {
+		select {
+		case str := <-event:
+			fmt.Println(str)
+			if str == "B1 FallingEdge" {
+				download()
+			}
+		}
+	}
+}
